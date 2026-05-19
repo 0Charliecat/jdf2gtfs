6e6023af2b01fd21c68816cd5b0224eae2f2d25e
Charlie <meow@charliecat.space>
2026-05-19 15:11:01 +0200

    Replace iconv-lite with windows-1250

--- Library/_app/_reusables/getContentsArray.ts
+++ Library/_app/_reusables/getContentsArray.ts
@@ -1,11 +1,8 @@
-import iconv from "iconv-lite"
 import Papa from 'papaparse';
+import * as windows1250 from 'windows-1250';
 
 export async function getContentsArray<T>(_file: Uint8Array, _headers: string[]) {
-	const _fileUTF8String = iconv.decode(
-		_file as unknown as Buffer,
-		'windows-1250'
-	)
+	const _fileUTF8String = windows1250.decode(_file);
 
 	let stringToParse = _headers.join(',') + "\r\n" + _fileUTF8String.trim().replaceAll('";', '"')
 
